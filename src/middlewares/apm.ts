19ac18420318d1183a141f48e0f33c0fd54d6cac
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2026-09-02 21:00:13 -0400

    Fixed the guard for bug vigil instrumenting its own writes
    The previous guard for vigil instrumenting its own write was taking the store object and making a copy of it ahead of time. But in a long term that didnt looked optimal.
    
    So i moved it inside the als.exit where store context is not allowed in return i placed a undefined guard in my pg instrumentation which guards this

--- src/middlewares/apm.ts
+++ src/middlewares/apm.ts
@@ -16,31 +16,37 @@ export default function apm(req: Request, res: Response, next: NextFunction) {
     res.on("finish", async () => {
       const end: bigint = process.hrtime.bigint();
       const durationInMs: number = Number(end - start) / 1e6;
-      try {
-        const requestText: string =
-          "INSERT INTO requests(method, route, status, duration_ms ) VALUES($1, $2, $3, $4) RETURNING request_id";
-        const requestValues: Array<string | number> = [
-          req.method,
-          routePattern(req),
-          res.statusCode,
-          durationInMs,
-        ];
-        const ctxQueries = [...(store.queries ?? [])];
-        const reqDbResponse = await pool.query(requestText, requestValues);
-        const requestId = reqDbResponse.rows[0]["request_id"];
-        const queryText = `
+
+      // wrapping the insert operation of apm inside als.exit
+      // the context is not available/undefined in here
+      // so that the contextStore gets undefined and does not pollute the store storage
+      als.exit(async () => {
+        try {
+          const requestText: string =
+            "INSERT INTO requests(method, route, status, duration_ms ) VALUES($1, $2, $3, $4) RETURNING request_id";
+          const requestValues: Array<string | number> = [
+            req.method,
+            routePattern(req),
+            res.statusCode,
+            durationInMs,
+          ];
+          const reqDbResponse = await pool.query(requestText, requestValues);
+          const ctxQueries = [...store.queries];
+          const requestId = reqDbResponse.rows[0]["request_id"];
+          const queryText = `
       INSERT INTO queries (request_id, query_text, duration_ms)
       SELECT $1, sql, duration_ms
       FROM unnest($2::text[], $3::float8[]) AS t(sql, duration_ms)
     `;
-        await pool.query(queryText, [
-          requestId,
-          ctxQueries.map((q) => q.sql),
-          ctxQueries.map((q) => q.durationInMs),
-        ]);
-      } catch (err) {
-        console.log(`Unexpected apm error ${err}`);
-      }
+          await pool.query(queryText, [
+            requestId,
+            ctxQueries.map((q) => q.sql),
+            ctxQueries.map((q) => q.durationInMs),
+          ]);
+        } catch (err) {
+          console.log(`Unexpected apm error ${err}`);
+        }
+      });
     });
     next();
   });
